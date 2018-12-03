c26442cd232d807e91abe3876711f7cad620aefb
Marc Lefrancois <mlefrancois@mirego.com>
2018-12-02 20:22:07 -0500

    Day 2 Part 1

---
+++ elixir/day2/day2.exs
@@ -0,0 +1,4 @@
+IO.puts("\n")
+IO.puts("Advent of code 2018 - Day 2")
+IO.puts("---------------------------")
+Day2.solve()
---
+++ elixir/day2/test/day2_test.exs
@@ -0,0 +1,93 @@
+defmodule Day2Part1Test do
+  use ExUnit.Case
+  import Day2.Part1
+
+  test "
+    Given a list of strings without repeating chars
+    Then the checksum is 0
+  " do
+    input = ~w(qwertyu asdfghj zxcvbnm)
+    assert solve(input) == 0
+  end
+
+  test "
+    Given a list of strings with one string with two a
+    Then the checksum is 0
+  " do
+    input = ~w(qwertyu aadfghj zxcvbnm)
+    assert solve(input) == 0
+  end
+
+  test "
+    Given a list of strings with one string with two a
+    and one with three b
+    Then the checksum is 1
+  " do
+    input = ~w(qwertyu aadfghj zxcvbbbnm)
+    assert solve(input) == 1
+  end
+
+  test "
+    Given a list of strings with one string with two a and 2 f
+    and one with three b
+    Then the checksum is 1
+  " do
+    input = ~w(qwertyu aadffghj zxcvbbbnm)
+    assert solve(input) == 1
+  end
+
+  test "
+    Given a list of strings with one string with 2 a and 2 f
+    and one with 3 b and 3 c
+    Then the checksum is 1
+  " do
+    input = ~w(qwertyu aadffghj zxcccvbbbnm)
+    assert solve(input) == 1
+  end
+
+  test "
+    Given a list of strings with one string with 2 a and 3 f
+    Then the checksum is 1
+  " do
+    input = ~w(qwertyu aadfffghj zxcvbnm)
+    assert solve(input) == 1
+  end
+
+  test "
+    Given a list of strings with one string with 2 a and 3 f
+    one with 3 chars
+    one with 2 chars
+    Then the checksum is 4
+  " do
+    input = ~w(qweeertyu aadfffghj zxccvbnm)
+    assert solve(input) == 4
+  end
+
+  test "
+    Given a list of strings with one string with 2 a and 3 f
+    one with 3 chars
+    one with 2 chars
+    one with 4 chars
+    Then the checksum is 4
+  " do
+    input = ~w(qweeertyu aadfffghj zxccvbnm qqqqertyu)
+    assert solve(input) == 4
+  end
+
+  test"
+    Given day 2 input
+    when solving
+    answer is 7105
+  " do
+    
+  end
+end
+
+defmodule Day2Part2Test do
+  use ExUnit.Case
+  import Day2.Part2
+
+  test "part2" do
+    # solve()
+  end
+end
